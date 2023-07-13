c86817090206e0d9addaeec88b3c12f7cb355164
Zetah07 <sebastiancastro0792@gmail.com>
2023-07-13 11:59:43 -0500

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import Navbar from '@/components/navbar'
 const inter = Urbanist({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Store',
-  description: 'A store for all your needs',
+  title: 'ZHORUS',
+  description: 'ZHORUS es tu tienda online de confianza para comprar proteínas y suplementos en España. Ofrecemos una amplia gama de productos de alta calidad diseñados para ayudarte a alcanzar tus objetivos de salud y fitness. Nuestro equipo de expertos en nutrición te brinda asesoramiento personalizado y recomendaciones basadas en tus necesidades individuales. ¡Descubre ZHORUS y potencia tus resultados hoy mismo!',
 }
 
 export default function RootLayout({
